fbf75a023eeb8402013d2aa4c75dd09160d28173
matthewstulz <stulzm2@nku.edu>
2019-06-24 19:28:12 -0700

    The sorting algorithm that we shouldn't speak of.

---
+++ src/sortingalgorithms/BubbleSort.java
@@ -0,0 +1,39 @@
+package sortingalgorithms;
+
+public class BubbleSort {
+
+    // Time complexity of O(n^2)
+    public static void bubbleSort(int[] array) {
+
+        int temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // Time complexity of O(n*n)
+    public static void bubbleSortV2(int[] array) {
+
+        int temp;
+        boolean swapped;
+        for (int i = 0; i < array.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
+}
